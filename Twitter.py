3f435e8374907880854b9967d49e17b5f743b075
loganjaglowski <30781051+loganjaglowski@users.noreply.github.com>
2021-04-17 17:49:50 -0400

    Add files via upload

--- Twitter.py
+++ Twitter.py
@@ -2,42 +2,44 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
-from nltk import FreqDist, classify, NaiveBayesClassifier
+from nltk import NaiveBayesClassifier
+from nltk import classify
 import tweepy
 from tweepy import OAuthHandler
 
-import re, string, random, nltk
+import re, string, random, nltk, operator
 nltk.download('twitter_samples')
 nltk.download('stopwords')
+nltk.download('movie_reviews')
 
 def remove_noise(tweet_tokens, stop_words = ()):
-
     cleaned_tokens = []
-
     for token, tag in pos_tag(tweet_tokens):
+
+        # First, work on the regex side. Make sure that no links, hashtags, or
+        # @usernames count as tokens
         token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+        token = re.sub('(@[A-Za-z0-9_]+)','', token)
+        token = re.sub('(#[A-Za-z0-9]+)','', token)
+        token = re.sub('&amp', 'and', token)
+        token = re.sub('\\\'[A-Za-z0-9_]+', '', token)
 
+        # Classify the tag as a noun, verb, or adjective/adverb that will fit with
+        # the WordNetLemmatizer
         if tag.startswith("NN"):
-            pos = 'n'
+            typeOfWord = 'n'
         elif tag.startswith('VB'):
-            pos = 'v'
+            typeOfWord = 'v'
         else:
-            pos = 'a'
+            typeOfWord = 'a'
+        wordNet = WordNetLemmatizer()
+        token = wordNet.lemmatize(token, typeOfWord)
 
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
-
+        # Check to see if token is valid and impactful towards judging sentiment
         if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
             cleaned_tokens.append(token.lower())
     return cleaned_tokens
-
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
-
 
 class TwitterClient(object):
     def __init__(self):
@@ -57,7 +59,7 @@ class TwitterClient(object):
             try:
                 fetched_tweets = self.api.search(q=query, count=count)
                 for tweet in fetched_tweets:
-                    parsed_tweet = {'text': tweet.text, 'author': tweet.author}
+                    parsed_tweet = {'text': tweet.text, 'author': tweet.author.id}
                     if tweet.retweet_count > 0:
                         if parsed_tweet not in tweets:
                             tweets.append(parsed_tweet)
@@ -68,77 +70,172 @@ class TwitterClient(object):
             except tweepy.TweepError as e:
                 print("Error : " + str(e))
 
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
+    def get_author_tweets(self, user):
+        try:
+            currentUser = self.api.get_user(user)
+            username = currentUser.screen_name
+            tweets = self.api.user_timeline(screen_name=username, count=50, include_rts=False)
+            return_text = []
+            for tweet in tweets:
+                parsed_tweet = {'text': tweet.text}
+                return_text.append(parsed_tweet)
+            return return_text
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
 
+    def get_username_tweets(self, user, search_count):
+        try:
+            tweets = self.api.user_timeline(screen_name=user, count=search_count, include_rts=True)
+            return_text = []
+            for tweet in tweets:
+                parsed_tweet = {'text': tweet.text}
+                return_text.append(parsed_tweet)
+            return return_text
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
 
 
 if __name__ == "__main__":
     api = TwitterClient()
     tweet_array = []
-
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    text = twitter_samples.strings('tweets.20150430-223406.json')
-    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
-
+    positive_cleaned = []
+    negative_cleaned = []
     stop_words = stopwords.words('english')
+    positive_reviews = nltk.corpus.movie_reviews.fileids(categories=["pos"])
+    negative_reviews = nltk.corpus.movie_reviews.fileids(categories=["neg"])
+    print("Tokenizing and removing noise from untokenized data sets")
+    for movie in positive_reviews:
+        text = nltk.corpus.movie_reviews.raw(movie)
+        positive_cleaned.append(remove_noise(word_tokenize(text)))
+    for movie in negative_reviews:
+        text = nltk.corpus.movie_reviews.raw(movie)
+        negative_cleaned.append(remove_noise(word_tokenize(text)))
+    positive_original = twitter_samples.tokenized('positive_tweets.json')
+    negative_original = twitter_samples.tokenized('negative_tweets.json')
+    print("Gathering and cleaning tokens from already tokenized data sets")
+    for tokens in positive_original:
+        positive_cleaned.append(remove_noise(tokens, stop_words))
 
-    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+    for tokens in negative_original:
+        negative_cleaned.append(remove_noise(tokens, stop_words))
+    positive_final = []
 
-    positive_cleaned_tokens_list = []
-    negative_cleaned_tokens_list = []
+    print("Classifying tokens as positive or negative")
+    for tweets in positive_cleaned:
+        for token in tweets:
+            addition = ({token: True}, "Positive")
+            positive_final.append(addition)
+    negative_final = []
+    for tweets in negative_cleaned:
+        for token in tweets:
+            addition = ({token: True}, "Negative")
+            negative_final.append(addition)
 
-    for tokens in positive_tweet_tokens:
-        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-    for tokens in negative_tweet_tokens:
-        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-    all_pos_words = get_all_words(positive_cleaned_tokens_list)
-
-    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
-
-    positive_dataset = [(tweet_dict, "Positive")
-                         for tweet_dict in positive_tokens_for_model]
-
-    negative_dataset = [(tweet_dict, "Negative")
-                         for tweet_dict in negative_tokens_for_model]
-
-    dataset = positive_dataset + negative_dataset
+    dataset = positive_final + negative_final
 
     random.shuffle(dataset)
 
-    train_data = dataset[:7000]
-    test_data = dataset[7000:]
-
+    # train_data = dataset[:7000]
+    # test_data = dataset[:7000]
+    train_data = dataset[:1200000]
+    test_data = dataset[1200000:]
+    print("Training the data\n")
     classifier = NaiveBayesClassifier.train(train_data)
+
+    print("Accuracy of the Naive Bayes Classifier is:", classify.accuracy(classifier, test_data))
+    print("All set to use!\n")
+
     still_searching = False
     while not still_searching:
-        search_input = input("What would you like to search? ")
-        is_number = False
-        query = 0
-        while not is_number:
-            test = input("How many tweets from this topic would you like to look at?  ")
-            try:
-                num = int(test)
-                print("Beginning query search...")
-                query = num
-                is_number = True
-            except ValueError:
-                print(
-                    "This is not a valid number.")
-        tweet_array = api.get_tweets(search_input, query)
-        increment = 1
-        for newTweet in tweet_array:
-            custom_tokens = remove_noise(word_tokenize(newTweet['text']))
-            print('TWEET #' + str(increment) + ': ')
-            print(newTweet['text'], classifier.classify(dict([token, True] for token in custom_tokens)))
-            print('\n')
-            increment += 1
-        search_input = input("Would you like to continue searching (Y/N)? ")
-        if search_input == "N":
-            still_searching = True
+        author_or_topic = input("Would you like to look at a user or topic? ")
+        if author_or_topic.lower() == "topic":
+            search_input = input("What would you like to search? ")
+            is_number = False
+            query = 0
+            while not is_number:
+                test = input("How many tweets from this topic would you like to look at?  ")
+                try:
+                    num = int(test)
+                    print("Beginning query search...")
+                    query = num
+                    is_number = True
+                except ValueError:
+                    print(
+                        "This is not a valid number.")
+            tweet_array = api.get_tweets(search_input, query)
+            increment = 1
+            positive_count = 0
+            negative_count = 0
+            score = 0
+            most_surprising = {}
+            for newTweet in tweet_array:
+                author_score = 0
+                custom_tokens = remove_noise(word_tokenize(newTweet['text']))
+                print('TWEET #' + str(increment) + ': ')
+                currentTweetSatisfaction = classifier.classify(dict([token, True] for token in custom_tokens))
+                if currentTweetSatisfaction == "Positive":
+                    positive_count += 1
+                    score = 0
+                else:
+                    negative_count += 1
+                    score = 1
+                print(newTweet['text'], '\nTHIS TWEET IS CLASSIFIED AS: ' + currentTweetSatisfaction)
+                author = api.get_author_tweets(newTweet['author'])
+                positive_increment = 0
+                negative_increment = 0
+                for tweet in author:
+                    author_token = remove_noise(word_tokenize(tweet['text']))
+                    if classifier.classify(dict([a_token, True] for a_token in author_token)) == "Positive":
+                        positive_increment += 1
+                    else:
+                        negative_increment += 1
+                print('Author has ' + str(positive_increment) + ' recent positive posts and '
+                      + str(negative_increment) + ' recent negative posts.')
+                if negative_increment == 0:
+                    author_score = 1
+                else:
+                    author_score = positive_increment / (positive_increment + negative_increment)
+                most_surprising[increment] = abs(score - author_score)
+                print('\n')
+                increment += 1
+            print("Number of positive posts: " + str(positive_count))
+            print("Number of negative posts: " + str(negative_count))
+            sorted_most_surprising = dict(sorted(most_surprising.items(), key=operator.itemgetter(1)))
+            print("Most surprising tweets to least surprising tweets: ")
+            print(list(sorted_most_surprising.keys()))
+            search_input = input("Would you like to continue searching (Y/N)? ")
+            if search_input == "N" or search_input == "n":
+                still_searching = True
+        elif author_or_topic.lower() == "user":
+            search_input = input("What is their username? ")
+            is_number = False
+            query = 0
+            while not is_number:
+                test = input("How many tweets from this topic would you like to look at?  ")
+                try:
+                    num = int(test)
+                    print("Beginning query search...")
+                    query = num
+                    is_number = True
+                except ValueError:
+                    print(
+                        "This is not a valid number.")
+            tweet_array = api.get_username_tweets(search_input, query)
+            increment = 1
+            posi_increment = 0
+            negative_increment = 0
+            for newTweet in tweet_array:
+                custom_tokens = remove_noise(word_tokenize(newTweet['text']))
+                print('TWEET #' + str(increment) + ': ')
+                currentTweetSatisfaction = classifier.classify(dict([token, True] for token in custom_tokens))
+                print(newTweet['text'], '\nTHIS TWEET IS CLASSIFIED AS: ' + currentTweetSatisfaction + '\n')
+                if currentTweetSatisfaction == "Positive":
+                    posi_increment += 1
+                else:
+                    negative_increment += 1
+                increment += 1
+            print("User has " + str(posi_increment) + " positive posts and "
+                  + str(negative_increment) + " negative posts.")
+            search_input = input("Would you like to continue searching (Y/N)? ")
+            if search_input == "N" or search_input == "n":
+                still_searching = True
